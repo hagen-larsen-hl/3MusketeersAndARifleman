70f27bd814ce2c742989b511f9231ed1420213bd
Bennett DenBleyer (Laptop) <bennett.denbleyker@usu.edu>
2022-02-24 11:11:36 -0700

    Basic working of User model

--- hustle/main/forms.py
+++ hustle/main/forms.py
@@ -6,15 +6,16 @@ from django.contrib.auth.models import User
 # Create your forms here.
 
 class NewUserForm(UserCreationForm):
+    email = forms.EmailField(label=("Email"), required=True, widget=forms.EmailInput)
+    first_name = forms.CharField(label="First Name", required=True)
+    last_name = forms.CharField(label="Last Name", required=True)
 
-	class Meta:
-		model = User
-		fields = ("username", "first_name", "last_name", "password1", "password2")
+    class Meta:
+        model = User
+        fields = ("email", "username", "first_name", "last_name",)
 
-	def save(self, commit=True):
-		user = super(NewUserForm, self).save(commit=False)
-		user.first_name = self.cleaned_data['first_name']
-		user.last_name = self.cleaned_data['last_name']
-		if commit:
-			user.save()
-		return user
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        if commit:
+            user.save()
+        return user
--- hustle/main/views.py
+++ hustle/main/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import AuthenticationForm
 def register_request(request):
 	if request.method == "POST":
 		form = NewUserForm(request.POST)
+		print(form)
 		if form.is_valid():
 			user = form.save()
 			login(request, user)
