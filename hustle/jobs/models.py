0f90d452a3f23f8c3a159a84817fc35a0f0ac662
some45bucks <70984053+some45bucks@users.noreply.github.com>
2022-02-24 03:36:26 -0700

    updated README a bit

--- hustle/jobs/models.py
+++ hustle/jobs/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 
+# This is the module for the jobtype object it isn't done yet
 class JobType(models.Model):
     typeName = models.CharField(max_length=200)
