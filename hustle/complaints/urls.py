84cf9560528f4fe03a6e861af0e8d0ff069aa9a4
hagen-larsen-hl <hagenjay7@gmail.com>
2022-03-01 08:30:51 -0700

    owners can now view all complaints by going to .../view/all/

--- hustle/complaints/urls.py
+++ hustle/complaints/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # path("", views.profile, name="profile"),
     path("create/", views.create, name="create"),
     path("view/", views.view, name="view"),
+    path("view/all/", views.viewAll, name="viewAll")
 ]
