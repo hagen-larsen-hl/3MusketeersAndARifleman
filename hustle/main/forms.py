798b6a06a028117dc3d001d59e4e7f547fe80bd1
some45bucks <70984053+some45bucks@users.noreply.github.com>
2022-03-01 06:10:03 -0700

    Added a profile and a way to eddit it
    You can edit most parts of the profile and deposit and withdraw money. some stuff is weird about the profile editing though

--- hustle/main/forms.py
+++ hustle/main/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.utils.datastructures import MultiValueDict
+
+from .models import UserData
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -6,16 +9,46 @@ from django.contrib.auth.models import User
 # Create your forms here.
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(label=("Email"), required=True, widget=forms.EmailInput)
+    email = forms.EmailField(label="Email", required=True, widget=forms.EmailInput)
     first_name = forms.CharField(label="First Name", required=True)
     last_name = forms.CharField(label="Last Name", required=True)
 
     class Meta:
         model = User
         fields = ("email", "username", "first_name", "last_name",)
-        
+
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
+
         if commit:
             user.save()
+            ud = UserData(user=user)
+            ud.save()
         return user
+
+
+class EditUser(forms.ModelForm):
+    email = forms.EmailField(label="Email", required=True, widget=forms.EmailInput)
+    first_name = forms.CharField(label="First Name", required=True)
+    last_name = forms.CharField(label="Last Name", required=True)
+
+    class Meta:
+        model = User
+        fields = ("email", "username", "first_name", "last_name",)
+
+    # This is some weird code that lets you magically set the initial values of the form
+    # I found it here >>> https://www.peterbe.com/plog/initial-values-bound-django-form-rendered
+    # I think it works by taking all the initial inputs and repackages them into a un-bounded form
+    # that then lets you populate the initial values
+    def __init__(self, data, **kwargs):
+        initial = kwargs.get("initial", {})
+        data = MultiValueDict({**{k: [v] for k, v in initial.items()}, **data})
+        super().__init__(data, **kwargs)
+
+
+class MoneyForm(forms.ModelForm):
+    money = forms.DecimalField(label="Amount", required=True, decimal_places=2)
+
+    class Meta:
+        model = UserData
+        fields = ("money",)
--- hustle/main/models.py
+++ hustle/main/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserData(models.Model):
-    user = models.OneToOneField(User, verbose_name="User", related_name="data", on_delete=models.CASCADE)
-
+    user = models.OneToOneField(User, verbose_name="User",primary_key=True ,related_name="data", on_delete=models.CASCADE)
+    money = models.DecimalField(max_digits=10,decimal_places=2,default=0.0)
 # Create your models here.
