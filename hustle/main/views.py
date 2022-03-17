64648a6b2823175089671cbad073521b7106ace1
some45bucks <70984053+some45bucks@users.noreply.github.com>
2022-03-16 23:26:05 -0600

    Added most of my tasks

--- hustle/main/views.py
+++ hustle/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import User
 from .forms import NewUserForm, MoneyForm, EditUser
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
@@ -6,35 +7,36 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 def register_request(request):
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			return redirect("main:profile")
-	else:
-		form = NewUserForm()
-	return render(request=request, template_name="main/register.html", context={"register_form": form})
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("main:profile")
+    else:
+        form = NewUserForm()
+    return render(request=request, template_name="main/register.html", context={"register_form": form})
 
 
 def login_request(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("main:profile")
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	else:
-		form = AuthenticationForm()
-	return render(request=request, template_name="main/login.html", context={"login_form":form})
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("main:profile")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request=request, template_name="main/login.html", context={"login_form": form})
+
 
 def logout_request(request):
     logout(request)
@@ -48,6 +50,17 @@ def profile(request):
     else:
         return redirect("main:login")
 
+def other_profile(request,user_id):
+    if request.user.is_authenticated:
+        if user_id == request.user.id:
+            return redirect("main:profile")
+
+        current_user = User.objects.get(pk=user_id)
+        return render(request, 'main/other_profile.html', {"user": current_user})
+    else:
+        return redirect("main:login")
+
+
 def deposit_money(request):
     if request.user.is_authenticated:
         form = MoneyForm(data=request.POST)
@@ -55,9 +68,10 @@ def deposit_money(request):
             request.user.data.money += form.cleaned_data['money']
             request.user.data.save()
             return redirect("main:profile")
-        return render(request, 'main/profile.html', {"deposit": True,"money_form": form})
+        return render(request, 'main/profile.html', {"deposit": True, "money_form": form})
     else:
         return redirect("main:login")
+
 
 def withdraw_money(request):
     if request.user.is_authenticated:
@@ -70,23 +84,18 @@ def withdraw_money(request):
     else:
         return redirect("main:login")
 
+
 def edit_user(request):
     if request.user.is_authenticated:
-        #this is how you pre populate the form
-        form = EditUser(data=request.POST,initial=
-        {"email": request.user.email,
-         "username": request.user.username,
-         "first_name": request.user.first_name,
-         "last_name": request.user.last_name,})
 
-        if form.is_valid():
-            request.user.email = form.cleaned_data['email']
-            request.user.first_name = form.cleaned_data['first_name']
-            request.user.last_name = form.cleaned_data['last_name']
-            request.user.save()
-            return redirect("main:profile")
+        form = EditUser(instance=request.user)
+
+        if request.method == "POST":
+            form = EditUser(data=request.POST,instance=request.user)
+            if form.is_valid():
+                form.save()
+                return redirect("main:profile")
         return render(request, 'main/profile.html', {"edit": True, "edit_form": form})
 
     else:
         return redirect("main:login")
-
