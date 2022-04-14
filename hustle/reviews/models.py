594d0fc30466153b6e1571692121cbafd86a8cc4
Bennett DenBleyer (Laptop) <bennett.denbleyker@usu.edu>
2022-04-13 18:52:14 -0600

    Show worker's average rating

--- hustle/reviews/models.py
+++ hustle/reviews/models.py
@@ -5,8 +5,8 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Review(models.Model):    
-    worker = models.ForeignKey(User, on_delete=models.RESTRICT, default=1, related_name="worker")
-    reviewer = models.ForeignKey(User, on_delete=models.RESTRICT, default=1, related_name="reviewer")
+    worker = models.ForeignKey(User, on_delete=models.RESTRICT, default=1, related_name="reviews")
+    reviewer = models.ForeignKey(User, on_delete=models.RESTRICT, default=1, related_name="reviews_left")
     rating = models.IntegerField("Rating (1-5, with 1 being the worst and 5 being the best)", default=5, validators=[MaxValueValidator(5), MinValueValidator(1)])
     comments = models.TextField("Additional Comments")
     create_date = models.DateField(default=timezone.now)
